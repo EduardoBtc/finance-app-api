7c37636143cf9c62ff36d4272fe2dc73253e99c9
inovacaoagil-droid <inovacaoagil@gmail.com>
2025-12-28 03:50:39 -0300

    feat:setup db base and setup init database

---
+++ src/db/postgres/migrations/exec.js
@@ -0,0 +1,24 @@
+import 'dotenv/config.js';
+import fs from 'fs';
+import path from 'path';
+import { pool } from '../helper.js';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export const execMigrations = async () => {
+    const client = await pool.connect();
+    try {
+        const filePath = path.join(__dirname, '01-init.sql');
+        const script = fs.readFileSync(filePath, 'utf8');
+        await client.query(script);
+        console.log('Migrations executed successfully');
+    } catch (error) {
+        console.error('Error executing migrations', error);
+    } finally {
+        await client.release();
+    }
+};
+
+execMigrations();
