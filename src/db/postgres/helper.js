5af54313eb1b0c0cd10fc23a1b172f59408e9ef7
inovacaoagil-droid <inovacaoagil@gmail.com>
2025-12-28 03:09:59 -0300

    chore:add pgsetup postgres dotenv

---
+++ src/db/postgres/helper.js
@@ -0,0 +1,20 @@
+import pg from 'pg';
+
+const { Pool } = pg;
+
+export const pool = new Pool({
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DB,
+    host: process.env.POSTGRES_HOST,
+});
+
+export const PostgresHelper = {
+    query: async (query, params) => {
+        const client = await pool.connect();
+        const result = await client.query(query, params);
+        await client.release();
+        return result;
+    },
+};
