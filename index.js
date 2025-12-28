390088e3def7f710421c5a3565f29d2ebbbc6fb9
inovacaoagil-droid <inovacaoagil@gmail.com>
2025-12-28 20:00:34 -0300

    feat: add CreateUserControler and params validations

--- index.js
+++ index.js
@@ -1,10 +1,30 @@
 import 'dotenv/config.js';
 import express from 'express';
+import { CreateUserController } from './src/controllers/create-user.js';
 
 const app = express();
 
 app.use(express.json());
 
-app.listen(process.env.PORT, () => {
-    console.log(`Server is running on port ${process.env.PORT}`);
+app.post('/api/users', async (request, response) => {
+    console.log('request', request);
+    const createUserController = new CreateUserController();
+    const { statusCode, body } = await createUserController.execute(request);
+    response.status(statusCode).json(body);
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
+// Tratamento de erros não capturados
+process.on('unhandledRejection', (reason, promise) => {
+    console.error('Unhandled Rejection at:', promise, 'reason:', reason);
+});
+
+process.on('uncaughtException', (error) => {
+    console.error('Uncaught Exception:', error);
+    process.exit(1);
 });
---
+++ src/controllers/create-user.js
@@ -0,0 +1,52 @@
+import { CreateUserUseCase } from '../use-cases/create-user.js';
+import { badRequest, created, internalServerError } from './helper.js';
+
+import validator from 'validator';
+
+export class CreateUserController {
+    async execute(httpRequest) {
+        try {
+            const params = httpRequest.body;
+
+            const requiredFields = [
+                'first_name',
+                'last_name',
+                'email',
+                'password',
+            ];
+
+            for (const field of requiredFields) {
+                if (!params[field] || params[field].trim().length === 0) {
+                    return badRequest({
+                        errorMessage: `Field ${field} is required`,
+                    });
+                }
+            }
+
+            const passwordIsValid = params.password.length >= 8;
+            if (!passwordIsValid) {
+                return badRequest({
+                    errorMessage:
+                        'Password must be at least 8 characters long.',
+                });
+            }
+
+            const isEmailValid = validator.isEmail(params.email);
+            if (!isEmailValid) {
+                return badRequest({
+                    errorMessage: 'Invalid email. Send a valid email address.',
+                });
+            }
+
+            const createUserUseCase = new CreateUserUseCase();
+            const createdUser = await createUserUseCase.execute(params);
+
+            return created(createdUser);
+        } catch (error) {
+            console.error(error);
+            return internalServerError({
+                errorMessage: 'Internal server error',
+            });
+        }
+    }
+}
