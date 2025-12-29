19f58d827d6c4aab9804129c62809410e9cae0d7
inovacaoagil-droid <inovacaoagil@gmail.com>
2025-12-29 18:54:53 -0300

    refactor: streamline user update process and maintain email uniqueness check

--- src/use-cases/update-user.js
+++ src/use-cases/update-user.js
@@ -1,49 +1,42 @@
 import { PostgresUpdateUserRepository } from '../repositories/postgres/update-user.js';
-import { internalServerError } from '../controllers/helper.js';
 import { PostgresGetUserByEmailRepository } from '../repositories/postgres/get-user-by-email.js';
 import { EmailAlreadyInUseError } from '../errors/users.js';
 import bcrypt from 'bcrypt';
 
 export class UpdateUserUseCase {
     async execute(userId, updateUserParams) {
-        try {
-            if (updateUserParams.email) {
-                const postgresGetUserByEmailRepository =
-                    new PostgresGetUserByEmailRepository();
+        if (updateUserParams.email) {
+            const postgresGetUserByEmailRepository =
+                new PostgresGetUserByEmailRepository();
 
-                const userWithProvidedEmail =
-                    await postgresGetUserByEmailRepository.execute(
-                        updateUserParams.email,
-                    );
-
-                if (userWithProvidedEmail)
-                    throw new EmailAlreadyInUseError(updateUserParams.email);
-            }
-
-            const updatedUser = {
-                ...updateUserParams,
-            };
-
-            if (updateUserParams.password) {
-                const passwordHash = await bcrypt.hash(
-                    updateUserParams.password,
-                    10,
+            const userWithProvidedEmail =
+                await postgresGetUserByEmailRepository.execute(
+                    updateUserParams.email,
                 );
-                updatedUser.password = passwordHash;
-            }
 
-            const postgresUpdateUserRepository =
-                new PostgresUpdateUserRepository();
-
-            const updatedUserResult =
-                await postgresUpdateUserRepository.execute(userId, updatedUser);
-
-            return updatedUserResult;
-        } catch (error) {
-            console.error(error);
-            return internalServerError({
-                errorMessage: 'Internal server error',
-            });
+            if (userWithProvidedEmail)
+                throw new EmailAlreadyInUseError(updateUserParams.email);
         }
+
+        const updatedUser = {
+            ...updateUserParams,
+        };
+
+        if (updateUserParams.password) {
+            const passwordHash = await bcrypt.hash(
+                updateUserParams.password,
+                10,
+            );
+            updatedUser.password = passwordHash;
+        }
+
+        const postgresUpdateUserRepository = new PostgresUpdateUserRepository();
+
+        const updatedUserResult = await postgresUpdateUserRepository.execute(
+            userId,
+            updatedUser,
+        );
+
+        return updatedUserResult;
     }
 }
